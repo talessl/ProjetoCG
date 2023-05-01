7f89c03247f8405a97b4343b231ee5810c194d55
Tales <78726974+talessl@users.noreply.github.com>
2023-04-30 23:52:57 -0300

    Update ilha.cpp

--- ilha.cpp
+++ ilha.cpp
@@ -1,36 +1,25 @@
-//Compilar: g++ -o ilha ilha.cpp -lGL -lGLU -lglut
-//rodar: ./ilha
-
-
-#include <SDL2/SDL.h>  
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/freeglut.h>
-#include <cmath>
+#include <math.h>
 #include <stdio.h>
 
 
-bool mouseIn = false;
-float posCameraX, posCameraY , posCameraZ ; 
-float camYaw = 0.0;
-float camPitch = 0.0;
-
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
+float posCameraX,posCameraY,posCameraZ;
+float anguloCamera = 0.0;
+float posXteaPot = -0.8, posYteaPot = 0.2, posZteaPot = 0.2;
+float velXteaPot = 0.0, velYteaPot = 0.0, velZteaPot = 0.0;
+float posXteaPot_2 = -0.8, posYteaPot_2 = 0.2, posZteaPot_2 = -0.2;
+float velXteaPot_2 = 0.0, velYteaPot_2 = 0.0, velZteaPot_2 = 0.0;
 
 
-void lockCamera();
-void moveCamera(float, float);
-void moveCameraUp(float, float);
-//void control();
-void updateCamera();
 
-void init(void) 
+
+void init (void) 
 {
-   glClearColor (1.0, 1.0, 1.0, 1.0);
-   
-        // Habilita a definição da cor do material a partir da cor corrente
+    glClearColor (1.0, 1.0, 1.0, 0.0);
+    
+		// Habilita a definição da cor do material a partir da cor corrente
         glEnable(GL_COLOR_MATERIAL);
         //Habilita o uso de iluminação
         glEnable(GL_LIGHTING);  
@@ -39,13 +28,30 @@ void init(void)
         // Habilita o depth-buffering
         glEnable(GL_DEPTH_TEST);
  
-        // Habilita o modelo de colorização de Gouraud
-        glShadeModel(GL_FLAT);
-        posCameraX = 0.3;
-		posCameraY = 0.1;
-		posCameraZ = 0;
+        // Habilita o modelo de colorização
+        glShadeModel(GL_SMOOTH);
 
+    
+    glLoadIdentity();
+    posCameraX = 0.3;
+	posCameraY = 0.1;
+	posCameraZ = 0.0;
+	
+	//movimento do teaPot
+	velXteaPot = 0.1;
+	velYteaPot = 0.2;
+	velZteaPot = 0.3;
+	
+	//movimento do teaPot_2
+	velXteaPot_2 = 0.1;
+	velYteaPot_2 = 0.2;
+	velZteaPot_2 = 0.3;
+    
+
+   
 }
+
+
 
 void DefineIluminacao (void)
 {
@@ -73,134 +79,37 @@ void DefineIluminacao (void)
         glLightfv(GL_LIGHT0, GL_DIFFUSE, luzDifusa );
         glLightfv(GL_LIGHT0, GL_SPECULAR, luzEspecular );
         glLightfv(GL_LIGHT0, GL_POSITION, posicaoLuz );   
-        }
+ }
 
 
-//camera
 
-
-	
-/*	
-
-void control(float moveVelocity, float mouseVelocity, bool mi){
-		if(mi){
-		int MidX = 320;
-		int MidY = 240;
-		SDL_ShowCursor(SDL_DISABLE);
-		int tmpX, tmpY;
-		SDL_GetMouseState(&tmpX, &tmpY);
-		camYaw += mouseVelocity*(MidX-tmpX);
-		camPitch += mouseVelocity*(MidY-tmpY);
-		lockCamera();
-		SDL_WarpMouse(MidX,MidY);
-		Uint8* state = SDL_GetKeyState(NULL);
-		if(state[SDLK_w]){
-			if(camPitch!=90 && camPitch!=-90){
-			moveCamera(moveVelocity,0.0);
-			moveCameraUp(moveVelocity, 0.0);
-			}
-			}
-			else if(state[SDLK_s]){
-				if(camPitch!=90 && camPitch!=-90){
-				moveCamera(moveVelocity,180.0);
-				moveCameraUp(moveVelocity, 180.0);
-				}
-				}
-				else if(state[SDLK_a]){
-					moveCamera(moveVelocity,90.0);
-					}
-					else if(state[SDLK_d]){
-						moveCamera(moveVelocity,270);
-						}
-					}
-		glRotatef(-camPitch,1.0,0.0,0.0);
-		glRotated(-camYaw,0.0,1.0,0.0);
-	}
-*/
-void updateCamera(){
-		
-		glTranslatef(-posCameraX, -posCameraY, -posCameraZ);
-}
-
-void moveCamera(float distance, float direction){
-
-	float rad = (camYaw + direction)*M_PI/180.0;
-	posCameraX -= sin(rad)*distance;
-	posCameraY -= cos(rad)*distance;
-}
-
-void moveCameraUp(float distance, float direction){
-	
-	float rad = (camPitch + direction) * M_PI/180.0;
-	posCameraY += sin(rad)*distance;
-	}
-
-void lockCamera(){
-	if(camPitch>90)
-	camPitch=90;
-	if(camPitch<-90)
-	camPitch=-90;
-	if(camYaw<0.0)
-	camYaw+=360.0;
-	if(camYaw>360.0)
-	camYaw>360.0;
-	}
-//camera
-
- 
-// Função responsável pela especificação dos parâmetros de iluminação
-
-void specialKeys(int key, int x, int y)
+void keyPressed(unsigned char key, int x, int y) 
 {
-	int moveVelocity = 0.2;
-	int mouseVelocity = 0.2;
-	
+   float angulo = 2*M_PI/180;
+   
    switch (key) {
-	   
-		case GLUT_KEY_RIGHT : 
-            moveCamera(moveVelocity,270);                 
-            break; 
+       case 'a': //rotacao em torno de x
+             posCameraX =  posCameraX*cos(-angulo) + posCameraZ*sin(-angulo); 
+            posCameraZ = -posCameraX*sin(-angulo) + posCameraZ*cos(-angulo);
+            break;
 
-      case GLUT_KEY_LEFT : 
-            moveCamera(moveVelocity,90.0);                     
-            break; 
-            
-         
-       case GLUT_KEY_UP : 
-           if(camPitch!=90 && camPitch!=-90){
-			moveCamera(moveVelocity,0.0);
-			moveCameraUp(moveVelocity, 0.0);}                    
-            break;    
-        case GLUT_KEY_DOWN : 
-            if(camPitch!=90 && camPitch!=-90){
-				moveCamera(moveVelocity,180.0);
-				moveCameraUp(moveVelocity, 180.0);}                     
-            break;    
-		
-			
+      case 'd': 
+          case GLUT_KEY_RIGHT : //rotacao em torno de x
+            posCameraX =  posCameraX*cos(angulo) + posCameraZ*sin(angulo); 
+            posCameraZ = -posCameraX*sin(angulo) + posCameraZ*cos(angulo);                      
+            break;
+
+        case 's': //rotacao em torno de z
+            posCameraY =  posCameraY*cos(-angulo) + posCameraZ*sin(-angulo); 
+            posCameraZ = -posCameraY*sin(-angulo) + posCameraZ*cos(-angulo);
+            break;
+
+        case 'w': //rotacao em torno de x
+            posCameraY =  posCameraY*cos(angulo) + posCameraZ*sin(angulo); 
+            posCameraZ = -posCameraY*sin(angulo) + posCameraZ*cos(angulo);
+            break;      
    }
-   
-    glRotatef(-camPitch,1.0,0.0,0.0);
-	glRotated(-camYaw,0.0,1.0,0.0);
-	glutPostRedisplay();
-}
-
-// Função que desenha o plano
-void desenhaAgua() {
-	
-    
-   glBegin(GL_QUAD_STRIP);
-   
-	glColor3f(0.0, 0.0, 1.0);
-	glVertex3d(-1.0, 0.0, -1.0);
-	glVertex3d(-1.0, 0.0, 1.0);
-	glVertex3d(1.0, 0.0, -1.0);
-	glVertex3d(1.0, 0.0, 1.0);
-	
-	
-     
-   glEnd();
-   
+   glutPostRedisplay();
 }
 
 void desenhaTerra(){
@@ -242,8 +151,9 @@ void desenhaTerra(){
 	glVertex3f(0.8, 0.1, 0.8);
 	glVertex3f(0.8, 0.1, -0.8);
 	glEnd();
-	/*glBegin(GL_QUADS);			// Face inferior
-	glNormal3f(0.0, -1.0, 0.0); 	
+	
+	/*glBegin(GL_QUADS);		// Face inferior	
+	glNormal3f(0.0, -1.0, 0.0);
 	glVertex3f(-0.8, 0, -0.8);
 	glVertex3f(0.8, 0, -0.8);
 	glVertex3f(0.8, 0, 0.8);
@@ -252,73 +162,166 @@ void desenhaTerra(){
 
 	
 }
+
+void desenhaAgua() {
 	
-	void reshape(int w, int h)
+    
+   glBegin(GL_QUAD_STRIP);
+   
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3d(-1.0, 0.0, -1.0);
+	glVertex3d(-1.0, 0.0, 1.0);
+	glVertex3d(1.0, 0.0, -1.0);
+	glVertex3d(1.0, 0.0, 1.0);
+	
+	
+     
+   glEnd();
+   
+}
+
+void desenhaLinhas(){
+	glBegin(GL_LINES);
+        glColor3f (1.0, 0.0, 0.0);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(1.0, 0.0, 0.0);
+        
+        glColor3f (0.0, 1.0, 0.0);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.0, 1.0, 0.0);
+      
+        glColor3f (0.0, 0.0, 1.0);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.0, 0.0, 1.0);    
+    glEnd();
+}
+
+void desenhaCubeAzul(){
+
+glTranslatef(-0.4, 0.2, -0.4); 
+glColor3f(0.0,0.0,1.0);
+glutSolidCube(0.1);
+
+}
+
+void desenhaCubeRosa(){
+
+glTranslatef(0.4, 0.2, 0.4); 
+glColor3f(1.0,0.5,0.5);
+glutSolidCube(0.1);
+
+}
+
+void desenhaTeaPot(){
+glTranslatef(posXteaPot, posYteaPot, posZteaPot); 
+
+
+glColor3f(1.0,0.0,0.0);
+glutSolidTeapot(0.1);
+glutWireSphere(0.1,6,6);
+
+
+}
+
+void desenhaTeaPot_2(){
+glTranslatef(posXteaPot_2, posYteaPot_2, posZteaPot_2); 
+
+
+glColor3f(1.0,1.0,0.0);
+glutSolidTeapot(0.1);
+glutWireSphere(0.1,6,6);
+
+
+}
+
+
+
+void update(int value) {
+    // Atualiza a posição do teapot
+    if(posXteaPot < 0.8){
+	posXteaPot += velXteaPot;
+	posXteaPot_2 += velXteaPot_2;}
+	else{
+	posXteaPot -= 1.5;
+	posXteaPot_2 -= 1.5;
+}
+    glutTimerFunc(1000, update, 0);
+    glutPostRedisplay();
+}
+
+void reshape(int w, int h)
 {
    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
    
    glMatrixMode(GL_PROJECTION);
    glLoadIdentity();
    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+   
 
    glMatrixMode(GL_MODELVIEW);
    glLoadIdentity();
-   DefineIluminacao();
 }
 
-//exibe a cena
-void display() {
+
+void display(void)
+{
+	
+	
+	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	DefineIluminacao();	
 	
 	
 	
-    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    DefineIluminacao();	
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+	gluLookAt (posCameraX, posCameraY, posCameraZ, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 	
-    
-    gluLookAt (posCameraX, posCameraY, posCameraZ, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-	//control(0.2,0.2,mouseIn);
-	updateCamera();
-    desenhaAgua();
-    desenhaTerra();
-    glBegin(GL_LINES);
-        glColor3f (1.0, 0.0, 0.0);//vermelho x
-        glVertex3f(0.0, 0.0, 0.0);
-        glVertex3f(1.0, 0.0, 0.0);
-        
-        glColor3f (0.0, 1.0, 0.0); //verde y
-        glVertex3f(0.0, 0.0, 0.0);
-        glVertex3f(0.0, 1.0, 0.0);
-      
-        glColor3f (0.0, 0.0, 1.0);//azul z
-        glVertex3f(0.0, 0.0, 0.0);
-        glVertex3f(0.0, 0.0, 1.0);    
-    glEnd();
+	desenhaAgua();
+	desenhaTerra();
+	
+	glPushMatrix();
+	desenhaTeaPot();
+	glPopMatrix();
+	
+	glPushMatrix();
+	desenhaTeaPot_2();
+	glPopMatrix();
+	
+	glPushMatrix();
+	desenhaCubeAzul();
+	glPopMatrix();
+	
+	glPushMatrix();
+	desenhaCubeRosa();
+	glPopMatrix();
+	
+	glPushMatrix();
+	desenhaLinhas();
+	glPopMatrix();
+	
+	
     glutSwapBuffers();
 }
 
 
 
 
-
-
-// Função principal
-int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition (100, 100);
-    glutCreateWindow("Plano da Cena");
-    
-    init ();
+int main(int argc, char** argv)
+{	
 	
-   glutDisplayFunc(display); 
-   glutSpecialFunc(specialKeys);
+		
+   glutInit(&argc, argv);
+   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+   glutInitWindowSize (500, 500); 
+   glutInitWindowPosition (100, 100);
+   glutCreateWindow (argv[0]);
+   init ();
+   
+   
+   glutDisplayFunc(display);
+   glutKeyboardFunc(keyPressed);
    glutReshapeFunc(reshape);
-   
-   
+   glutTimerFunc(0, update, 0);
    glutMainLoop();
-   
-   return 0;
+   return 0;  
 }
